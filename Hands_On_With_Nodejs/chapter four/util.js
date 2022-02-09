6e05d0e65a80c15045de54c7789f3601b49cee8d
dhyey0011 <dhyey.dave@eronkan.com>
2022-02-09 11:13:24 +0530

    All about Util

---
+++ Hands_On_With_Nodejs/chapter four/util.js
@@ -0,0 +1,7 @@
+var util = require('util');
+util.log('Hello');
+
+var util = require('util');
+var a = {1: true, 2: false};
+console.log(util.inspect(a));
+util.inspect(object, showHidden, depth = 2, showColors);

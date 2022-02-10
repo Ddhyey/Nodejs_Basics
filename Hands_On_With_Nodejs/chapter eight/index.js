8a5249b1f872a87dfcbd4d4d2d457121e3ccc88f
dhyey0011 <dhyey.dave@eronkan.com>
2022-02-10 13:10:25 +0530

    file to run all exercises

---
+++ Hands_On_With_Nodejs/chapter eight/index.js
@@ -0,0 +1,12 @@
+const Ex1 = require('./ex1');
+const Ex2 = require('./ex2');
+const Ex3 = require('./ex3');
+const Ex4 = require('./ex4');
+const Ex5 = require('./ex5');
+const Ex6 = require('./ex6');
+Ex1();//immmediate calling ex1
+Ex2();//immmediate calling ex2
+Ex3();//immmediate calling ex3
+Ex4();//immmediate calling ex4
+Ex5();//immmediate calling ex5
+Ex6();//immmediate calling ex6

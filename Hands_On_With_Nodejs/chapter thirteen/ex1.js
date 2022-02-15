b8ab8fcab7e3ed0387e0578c33a2e6ddf0c4770d
dhyey0011 <dhyey.dave@eronkan.com>
2022-02-15 10:42:55 +0530

    Exercise 1

---
+++ Hands_On_With_Nodejs/chapter thirteen/ex1.js
@@ -0,0 +1,8 @@
+//Create a UDP server that echoes the messages ot receives back into the origin socket.
+var dgram = require('dgram');
+
+var socket = dgram.createSocket('udp4', function(message, rinfo){
+    console.log(rinfo);
+    socket.send(message, 0, message.length, rinfo.port, rinfo.address);
+});
+socket.bind(4002);

45187543cfaeeb7ebb620bdc38e2a8d29777262a
Ddhyey <dhyey.dave@eronkan.com>
2022-02-15 17:32:08 +0530

    All about Debugging

---
+++ Hands_On_With_Nodejs/chapter nineteen/start.js
@@ -0,0 +1,20 @@
+var obj = {a: 1, b: 2};
+console.log(obj);
+
+//using the node debugger
+var a = 1;
+debugger;
+var b = a + 1;
+
+//All the debugger commands
+/*
+node debug
+run
+list
+print a
+next
+continue
+kill
+*/
+
+//Node Inspector

c632df5950755762dc321bbfa05832010b7f4e52
Ddhyey <dhyey.dave@eronkan.com>
2022-02-19 16:13:21 +0530

    A Basic HTTP Server

---
+++ NodeBeginner/Chapter-1/httpServer.js
@@ -0,0 +1,5 @@
+var http =  require("http");
+http.createServer(function(request, response){
+    response.writeHead(200,{"content-type":"text/plain"});
+    response.write("Hello World");
+    response.end();}).listen(8080);

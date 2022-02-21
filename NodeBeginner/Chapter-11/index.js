d82b3e1b8340c28a300bc56dff4548fb0f0b72c5
Ddhyey <dhyey.dave@eronkan.com>
2022-02-21 13:19:54 +0530

    Deleted files

--- NodeBeginner/Chapter-11/index.js
+++
@@ -1,8 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./reqHandlers");
-var handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-server.start(router.route, handle);

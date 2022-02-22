f74ae194c5d76202e13c220fc0671f9b38c1c1bd
Ddhyey <dhyey.dave@eronkan.com>
2022-02-22 11:29:28 +0530

    Making Modules

--- Hands_On_With_Nodejs/chapter eighteen/index.js
+++ Hands_On_With_Nodejs/chapter eighteen/index.js
@@ -9,14 +9,3 @@ console.log(myModule.functionB());
 var Line = require('./line');
 var line = Line.create(2, 4, 10, 15);
 console.log('This line length is ' + line.length());
-//creating a psuedoClass
-var util = require('util'),
-    EventEmitter = require('events').EventEmitter;
-util.inherits(Line, EventEmitter);
-
-Line.prototype.length = function () {
-    return Math.sqrt(
-        Math.pow(Math.abs(this.x1 - this.x2), 2) +
-        Math.pow(Math.abs(this.y1 - this.y2), 2)
-    );
-};
